8ff05047ec42dc1575f5d8dade054cf93243b94b
pis00 <pisano.luca37@gmail.com>
2025-11-20 11:55:39 +0100

    fix

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig } from "vite";
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-    plugins: [basicSsl()],
-    server: {
-        https: true,
-        host: '0.0.0.0',
+  plugins: [basicSsl()],
+  server: {
+    https: true,
+    host: "0.0.0.0",
+  },
+  base: "/WebXR_Exercise1/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        scene1: "scene1.html",
+        scene2: "scene2.html",
+        progetto: "progetto.html",
+      },
     },
-    base: '/WebXR_Exercise1/',
+  },
 });
